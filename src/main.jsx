295918eae02a3db074504365d5a9f183ada553b6
sushant roy <sushantroyofficial@gmail.com>
2024-11-17 12:43:38 +0530

    changed route code

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,58 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+// import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, HashRouter } from 'react-router-dom';
+// import Layout from './Layout';
+// import { Home, Cart, Productlist } from './components';
+// import store from './store/store';
+// import { Provider } from 'react-redux';
+// import Products from './datas/items'
+
+// const routers = createBrowserRouter(
+//   createRoutesFromElements(
+//     <>
+//       <Route path='/' element={<Home />} />
+//       <Route element={<Layout />}>
+//         <Route path='products' element={<Productlist productsProp={Products} />} />
+//         <Route path='cart' element={<Cart />} />
+//       </Route>
+//     </>
+//   )
+// );
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <HashRouter>
+//         <RouterProvider router={routers} />
+//       </HashRouter>
+//     </Provider>
+//   </StrictMode>
+// );
+
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import { Home, Cart, Productlist } from './components';
 import store from './store/store';
 import { Provider } from 'react-redux';
-import Products from './datas/items'
-
-const routers = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path='/' element={<Home />} />
-      <Route element={<Layout />}>
-        <Route path='products' element={<Productlist productsProp={Products} />} />
-        <Route path='cart' element={<Cart />} />
-      </Route>
-    </>
-  )
-);
+import Products from './datas/items';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <HashRouter>
-        <RouterProvider router={routers} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<Layout />}>
+            <Route path="products" element={<Productlist productsProp={Products} />} />
+            <Route path="cart" element={<Cart />} />
+          </Route>
+        </Routes>
       </HashRouter>
     </Provider>
   </StrictMode>
 );
-
-
